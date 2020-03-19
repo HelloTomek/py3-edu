618491c9e832b06d4d89c9aa63476c442f576e11
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-20 00:19:41 +0100

    Create komentarze_i_zmienne.py

---
+++ python_raw/komentarze_i_zmienne.py
@@ -0,0 +1,17 @@
+## 0. Komentarze Python
+ 
+"""
+W pythonie zobaczymy komentarze typu multiline trzykrotny cudzyslow, stosujemy rowniez ''' jako docstring '''.
+Aby wykomentowac skrypt, polecam stosowac skroty klawiszowe, np. w IDLE:
+
+- Single line comment. Ctrl + 1.
+- Multi-line comment select the lines to be commented. Ctrl + 4.
+- Unblock Multi-line comment. Ctrl + 5.
+"""
+
+## 1. Zmienne i ich typy
+
+# ZMIENNE ZASADY:
+#  - W nazwach zmiennych rozróżniana jest wielkość liter (nazwa i NAZWA to różne zmienne)
+#  - Muszą one zaczynać się od litery lub znaku podkreślenia
+#  - Może mieć liczby, ale nie może zaczynać się od cyfry.
