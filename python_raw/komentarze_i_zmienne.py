28ccd7d8de0564e94f05e8beb696401a3b101a42
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-20 12:04:16 +0100

    Update komentarze_i_zmienne.py

--- python_raw/komentarze_i_zmienne.py
+++ python_raw/komentarze_i_zmienne.py
@@ -1,20 +1,26 @@
 ## 0. Komentarze Python
  
 """
-W pythonie zobaczymy komentarze typu multiline trzykrotny cudzyslów, 
+W pythonie zobaczymy komentarze typu multiline, używany jest w tym celu trzykrotny cudzyslów.
 
-Aby wykomentowac skrypt, polecam stosowac skroty klawiszowe, np. w IDLE:
+Nie jest to optymalny sposob. Cytuje Guido van Rossum, the creator of Python : 
+>> Python does not support multiline comments like C/C++ or Java. 
+>> However there is nothing to stop you to use multi-line docstrings as multiline comments."
+
+Natomiast aby wykomentowac skrypt, polecam stosowac skroty klawiszowe, np. w IDLE:
 - Single line comment. Ctrl + 1.
 - Multi-line comment, zaznacz kod który chcesz wykomentować. Ctrl + 4.
 - Unblock Multi-line comment. Ctrl + 5.
 """
 
 # drugi przyklad
+
 ''' 
-spotkamy się takze z pow. stosowaną notacją do wielowierszowych komentarzy - uzywajac pojedynczego cudzysłowu, 
+spotkamy się takze z powyżej stosowaną notacją do wielowierszowych komentarzy - uzywajac pojedynczego cudzysłowu, 
 
 rowniez moze być on używany jako docstring, więcej o docstring:
 https://www.python.org/dev/peps/pep-0257/
+
 '''
 
 ## 1. Zmienne i ich typy
@@ -26,4 +32,5 @@ https://www.python.org/dev/peps/pep-0257/
 
 ## 1. a) Ciągi znaków - https://github.com/HelloTomek/IwB/blob/master/python_raw/strings.py
 ## 1. b) Typy liczbowe - https://github.com/HelloTomek/IwB/blob/master/python_raw/numbers.py
-##
+## 1. c) Listy -  https://github.com/HelloTomek/IwB/blob/master/python_raw/lists.py
+## 1. d) Tuples i Sets - https://github.com/HelloTomek/IwB/blob/master/python_raw/tuples_sets.py
