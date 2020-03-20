766c699c571546af1b7b8393dcf6bb9c270ee50b
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-20 12:03:12 +0100

    Create tuples_sets.py

---
+++ python_raw/tuples_sets.py
@@ -0,0 +1,5 @@
+# Tuple to kolekcja, która jest uporządkowana i niezmienna. Umożliwia duplikaty członków.
+ 
+
+
+# Zestaw to kolekcja, która jest nieuporządkowana i nieindeksowana. Brak zduplikowanych członków.
