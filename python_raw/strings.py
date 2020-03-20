470038c16c26ae8e812ff8a2abf2a611ed2ed282
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-20 11:23:07 +0100

    Create strings.py

---
+++ python_raw/strings.py
@@ -0,0 +1,11 @@
+# Ciągi znaków w pythonie są otoczone pojedynczymi lub podwójnymi cudzysłowami. Spójrzmy na formatowanie ciągów i niektóre metody na danych typu String.
+
+## Formatowanie ciągów znaków
+
+# Konkatencja - w programowaniu oznacza łączenie dwóch wyrażeń 
+
+# Argumenty pozycyjne
+
+# F-Strings (w Python pow. 3.6)
+
+## Metody ciągów znaków
