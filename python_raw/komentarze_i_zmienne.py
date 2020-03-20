7c7e2846fe42e888205f9a48f3b68dad97c52d37
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-20 11:47:41 +0100

    Update komentarze_i_zmienne.py

--- python_raw/komentarze_i_zmienne.py
+++ python_raw/komentarze_i_zmienne.py
@@ -1,14 +1,21 @@
 ## 0. Komentarze Python
  
 """
-W pythonie zobaczymy komentarze typu multiline trzykrotny cudzyslow, stosujemy rowniez ''' jako docstring '''.
+W pythonie zobaczymy komentarze typu multiline trzykrotny cudzyslów, 
 
 Aby wykomentowac skrypt, polecam stosowac skroty klawiszowe, np. w IDLE:
 - Single line comment. Ctrl + 1.
 - Multi-line comment, zaznacz kod który chcesz wykomentować. Ctrl + 4.
 - Unblock Multi-line comment. Ctrl + 5.
-
 """
+
+# drugi przyklad
+''' 
+spotkamy się takze z pow. stosowaną notacją do wielowierszowych komentarzy - uzywajac pojedynczego cudzysłowu, 
+
+rowniez moze być on używany jako docstring, więcej o docstring:
+https://www.python.org/dev/peps/pep-0257/
+'''
 
 ## 1. Zmienne i ich typy
 
@@ -16,3 +23,7 @@ Aby wykomentowac skrypt, polecam stosowac skroty klawiszowe, np. w IDLE:
 #  - W nazwach zmiennych rozróżniana jest wielkość liter (nazwa i NAZWA to różne zmienne)
 #  - Muszą one zaczynać się od litery lub znaku podkreślenia
 #  - Może mieć liczby, ale nie może zaczynać się od cyfry.
+
+## 1. a) Ciągi znaków - https://github.com/HelloTomek/IwB/blob/master/python_raw/strings.py
+## 1. b) Typy liczbowe - https://github.com/HelloTomek/IwB/blob/master/python_raw/numbers.py
+##
