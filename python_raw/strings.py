0e7a8d83bca3a3f1365e34a1a1d3ac3ae4d1632d
HelloTomek <62406375+HelloTomek@users.noreply.github.com>
2020-03-29 23:31:50 +0200

    Update strings.py

--- python_raw/strings.py
+++ python_raw/strings.py
@@ -107,3 +107,20 @@ print(hello.isalpha())
 
 # Output typ boolean - Wszystkie jako numeric
 print(hw.isnumeric())         # False
+
+## Znaki specjalne notacja backslesh'owa oraz hex, wyjasnienie: 
+# \a	  0x07	Alert
+# \b	  0x08	Backspace
+# \cx	 	      Control-x
+# \C-x	      Control-x
+# \e	  0x1b	Escape
+# \f	  0x0c	Formfeed
+# \M-\C-x	 	  Meta-Control-x
+# \n	  0x0a	Nowa linia
+# \nnn	      Osemkowa Notacja, gdzie n is in the range 0.7
+# \r	  0x0d	return
+# \s	  0x20	Spacja
+# \t	  0x09	Tab
+# \v	  0x0b	Vertical tab
+# \x	 	      Character x
+# \xnn	      Hexadecimal notation, where n is in the range 0.9, a.f, or A.F
